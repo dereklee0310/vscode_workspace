f2496188f5bd67d135bb0b96be09b562a14d0a44
Dereklee0310 <dereklee0310@gmail.com>
2021-12-15 16:04:05 +0800

    Assignment5 final refinement

--- data_structure/Assignment5_3.c
+++ data_structure/Assignment5_3.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <limits.h>
 
 #define MAX 1000000001
 
@@ -14,34 +15,37 @@ int *Dijkstra(int **adjMatrix, int vertexNum, int root);
 int getMin(int *distanceArr, int vertexNum, bool *isfound);
 
 // calculate the total distance from root to all vertices
-int getTotalDistance(int *distanceArr, int nameNum);
+int getTotalDistance(int *distanceArr, int vertexNum);
 
 int main() {
     int **adjMatrix;
     char names[2048][21];
-    int nameNum, edgeNum;
+    int vertexNum, edgeNum;
     int vertex1, vertex2, distance;
     int totalDistance;
     int minDistance = -1;
     int minIdx = -1;
     int cnt = 1;
 
-    scanf("%d %d", &nameNum, &edgeNum);
-    while(nameNum != 0 && edgeNum != 0) {
-        adjMatrix = createMatrix(adjMatrix, nameNum);
-        for(int i = 0; i < nameNum; i++)
+    scanf("%d %d", &vertexNum, &edgeNum);
+    while(vertexNum != 0 && edgeNum != 0) {
+        adjMatrix = createMatrix(adjMatrix, vertexNum);
+        for(int i = 0; i < vertexNum; i++)
             scanf("%s", names[i]);
         for(int i = 0; i < edgeNum; i++) {
             scanf("%d %d %d", &vertex1, &vertex2, &distance);
+            // -1 because of the difference between array indices and vertex
+            vertex1--; 
+            vertex2--;
             adjMatrix[vertex1][vertex2] = distance;
             adjMatrix[vertex2][vertex1] = distance;
         }
 
-        int *distanceArr = (int *)malloc(sizeof(int) * nameNum);
+        int *distanceArr = (int *)malloc(sizeof(int) * vertexNum);
         minDistance = -1;
-        for(int i = 0; i < nameNum; i++) {
-            distanceArr = Dijkstra(adjMatrix, nameNum, i);
-            totalDistance = getTotalDistance(distanceArr, nameNum);
+        for(int i = 0; i < vertexNum; i++) {
+            distanceArr = Dijkstra(adjMatrix, vertexNum, i);
+            totalDistance = getTotalDistance(distanceArr, vertexNum);
             if(minDistance == -1 || totalDistance < minDistance) {
                 minIdx = i;
                 minDistance = totalDistance;
@@ -50,10 +54,13 @@ int main() {
         
         printf("Case #%d %s\n", cnt, names[minIdx]);
         cnt++;
-
-        scanf("%d %d", &nameNum, &edgeNum);
+        free(distanceArr);
+        
+        scanf("%d %d", &vertexNum, &edgeNum);
     }
-    // todo free memory
+    for(int i = 0; i < vertexNum; i++)
+        free(adjMatrix[i]);
+    free(adjMatrix);
 
     return 0;
 }
@@ -65,12 +72,9 @@ int **createMatrix(int **adjMatrix, int vertexNum)
         adjMatrix[i] = (int *)malloc(sizeof(int) * vertexNum);
 
     for(int i = 0; i < vertexNum; i++)
-        for(int j = 0; j < vertexNum; j++) {
-            if(i == j)  
-                adjMatrix[i][j] = 0;
-            else
-                adjMatrix[i][j] = MAX;
-        }
+        for(int j = 0; j < vertexNum; j++)
+            adjMatrix[i][j] = i == j ? 0 : MAX;
+
     return adjMatrix;
 }
 
@@ -91,11 +95,14 @@ int *Dijkstra(int **adjMatrix, int vertexNum, int root)
     distanceArr[root] = 0;
     for(int i = 0; i < vertexNum - 2; i++) {
         curPoint = getMin(distanceArr, vertexNum, isfound);
+        isfound[curPoint] = true;
         for(int j = 0; j < vertexNum; j++)
             if(!isfound[j])
                 if(distanceArr[curPoint] + adjMatrix[curPoint][j] < distanceArr[j])
                     distanceArr[j] = distanceArr[curPoint] + adjMatrix[curPoint][j];
     }
+
+    free(isfound);
 
     return distanceArr;
 }
@@ -103,7 +110,7 @@ int *Dijkstra(int **adjMatrix, int vertexNum, int root)
 int getMin(int *distanceArr, int vertexNum, bool *isfound)
 {
     int minPos = -1;
-    int min = MAX;
+    int min = INT_MAX;
 
     for(int i = 0; i < vertexNum; i++) {
         if(distanceArr[i] < min && !isfound[i]) {
@@ -115,11 +122,12 @@ int getMin(int *distanceArr, int vertexNum, bool *isfound)
     return minPos;
 }
 
-int getTotalDistance(int *distanceArr, int nameNum)
+int getTotalDistance(int *distanceArr, int vertexNum)
 {   
     int totalDistance = 0;
 
-    for(int i = 0; i < nameNum; i++)
+    for(int i = 0; i < vertexNum; i++)
         totalDistance += distanceArr[i];
+        
     return totalDistance;
 }
